011c55cb9eddabd14e9f89ce7cc99142eab16d1b
Youssef Wadie <youssefwadie14@gmail.com>
2022-01-08 18:32:11 +0200

    Scene background changed

--- Assets/Scripts/StoryHandler.cs
+++ Assets/Scripts/StoryHandler.cs
@@ -9,14 +9,9 @@ public class StoryHandler : MonoBehaviour
 {
     public VideoPlayer video;
 
-    void Start() {
-        
-    }
-
     public void Back()
     {
         video.Stop();
-
         SceneManager.LoadScene("Stories");
     }
     
@@ -26,19 +21,6 @@ public class StoryHandler : MonoBehaviour
     
     public void PauseVideo() {
         video.Pause();
-    }
-    public void PreviousStory() {
-        if(!(SceneManager.GetActiveScene().name == "Story1")){
-            video.Stop();
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-        }
-    }
-
-    public void NextStory() {
-        if(!(SceneManager.GetActiveScene().name == "Story4")){
-            video.Stop();
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
     }
 
 }
